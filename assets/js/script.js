6a66ab5aad32df2870013038832105efd9f22624
BarbierTheo <lebarbier.lh@gmail.com>
2025-01-14 01:51:02 +0100

    format

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,22 +34,22 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Le Havre&appid=9fcb5abd
         }
 
         // for (main of currentWeather.main) {
-            // console.log(main)
-            // Objet pas array ?
+        // console.log(main)
+        // Objet pas array ?
 
         // }
 
-        document.getElementById('temp').innerHTML = Math.round(10*currentWeather.main['temp'])/10 + "°c"
-        document.getElementById('feel_like').innerHTML = `Ressenti : ${Math.round(10*currentWeather.main['feels_like'])/10} °c`
-        document.getElementById('mintemp').innerHTML = `Min : ${Math.round(10*currentWeather.main['temp_min'])/10} °c`
-        document.getElementById('maxtemp').innerHTML = `Max : ${Math.round(10*currentWeather.main['temp_max'])/10} °c`
+        document.getElementById('temp').innerHTML = Math.round(10 * currentWeather.main['temp']) / 10 + "°c"
+        document.getElementById('feel_like').innerHTML = `Ressenti : ${Math.round(10 * currentWeather.main['feels_like']) / 10} °c`
+        document.getElementById('mintemp').innerHTML = `Min : ${Math.round(10 * currentWeather.main['temp_min']) / 10} °c`
+        document.getElementById('maxtemp').innerHTML = `Max : ${Math.round(10 * currentWeather.main['temp_max']) / 10} °c`
         document.getElementById('humidity').innerHTML = `${currentWeather.main['humidity']} °c`
 
-        const windspeed = Math.round(10*(currentWeather.wind['speed']*3600/1000)/10)
+        const windspeed = Math.round(10 * (currentWeather.wind['speed'] * 3600 / 1000) / 10)
         document.getElementById('wind').innerHTML = `${windspeed} km/h`
         document.getElementById('windimg').classList.add(`rotate-[${currentWeather.wind['deg']}deg]`)
 
-    
+
 
 
     })
